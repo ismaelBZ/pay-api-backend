2d62f3239335610eb2e588db87d01149419c004e
Ismael <ismaelbetzoletti@gmail.com>
2024-07-24 10:47:45 -0300

    changed front end requests

--- dist/app.js
+++ dist/app.js
@@ -7,9 +7,7 @@ const routes = require('./routes/index');
 const app = express();
 app.use(cors({
     origin: [
-        "http://127.0.0.1:5173",
-        "http://192.168.0.110:5173",
-        "http://localhost:5173"
+        "https://ismaelbz.github.io",
     ],
     optionsSuccessStatus: 200
 }));
